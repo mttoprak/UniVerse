3e43d6939b6d56af209e10c7c735f997773c4b64
mttoprak <mttoprak@outlook.com>
2026-04-13 05:35:03 +0300

    Implement backend structure with database connection, user model and type

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,55 @@
-import express, { Request, Response } from 'express';
+/*
+ * In index.ts we arrange the entire backend.
+ * We adjust cors in here.
+ * We set the port for the backend in here.
+ * We connect to the database in here.
+ * We WILL connect to the socket.io in here.
+ * We WILL route the routers in here.
+ * ...
+ *
+ */
+
+
+
+import express, { Request, Response } from 'express'; // I think we can rephrase this as
+// " import * as express from 'express' " but the first rule of the programing is never touch any code that works
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+dotenv.config();
 app.use(express.json());
+app.use(cors());
+
+
+const connect=async()=>{ //connection to the database
+    if (!process.env.MONGO_URI) {
+        console.error("Error: MONGO_URI environment variable is not defined.");
+        process.exit(1);
+    }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB Atlas");
+        console.log(`Backend server running on port ${PORT}`);
+    } catch (error) {
+        console.error("DB Connection Error:", error);
+    }
+}
+
+mongoose.connection.on("disconnected", () => { // the error message when database disconnection
+    console.log("mongoDB disconnected!");
+});
+
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('UniVerse Backend API Çalışıyor');
+    res.send('UniVerse Backend API working');
 });
 
 app.listen(PORT, () => {
-    console.log(`Sunucu http://localhost:${PORT} portunda başlatıldı.`);
+    connect() //database
+    console.log(`Server started working in http://localhost:${PORT}!`);
 });
---
+++ backend/types/user.types.ts
@@ -0,0 +1,42 @@
+/*
+ * In user.types.ts we define the types for using
+ * mongoose/mongodb models into typescript types
+ *
+ */
+
+
+import { Document, Types } from "mongoose"
+
+export interface IUser extends Document {
+    _id: Types.ObjectId
+
+    username?:      string
+    email:          string
+    edu_email?:     string
+    name:           string
+    surname:        string
+    birthdate?:     Date
+    telephone?:     string
+
+    password?:      string
+    googleId?:      string
+    profile_photo?: string
+
+    account_type:   "student" | "external"
+    auth_provider:  "local" | "google"
+    is_complete:    boolean
+    // is_verified:    boolean
+    // I didn't decide rather add this or not. AI still insist on it but, I feel we don't need
+
+    is_banned:      boolean
+    is_admin:       boolean
+
+    university?:        string
+    favourite_listings: Types.ObjectId[]
+    favourite_sellers:  Types.ObjectId[]
+    rating_sum:         number
+    rating_count:       number
+
+    createdAt: Date
+    updatedAt: Date
+}
