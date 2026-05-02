bc3319570ae002915ec2fc74a6640a9b2c8a08fb
mttoprak <mttoprak@outlook.com>
2026-05-02 20:58:26 +0300

    minor changes and details misc.router.ts and auth.validator.ts
    added cource on birthdate in

--- backend/src/routes/misc.router.ts
+++ backend/src/routes/misc.router.ts
@@ -3,6 +3,7 @@ import {getDistricts} from "../controllers/misc.controller";
 
 const router = Router()
 
+// GET /api/misc/districts/:city_id
 router.get("/districts/:city_id", getDistricts )
 
 export default router
